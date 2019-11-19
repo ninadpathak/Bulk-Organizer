ef71530df9ecedc8783ad44fa66426a2e602ee7c
Ninad Pathak <20905629+ninadpathak@users.noreply.github.com>
2019-11-19 11:24:10 +0530

    Created a method

--- main.py
+++ main.py
@@ -1,27 +1,17 @@
 # This script is created to automatically organize the shitloads of call recordings that I have
-
 import os, time, shutil, calendar
-#for i in range (1,20):
-#    f = open("Someshit_2019"+str(i)+"10_sommoreshit", "x+")
-#    f.write("Random")
-#    f.close
-dir = input("Enter the complete path: ") + "\\"
-files = os.listdir(dir)
-for f in files:
-    # moddate = time.ctime(os.path.getctime(f))
-    #  print(repr(moddate))
-    # splitdate = moddate.split()
-    # print(splitdate[1])
+def organizeFiles(fileName):
     ogname = dir + f
     print(ogname)
+
     #Splitting filenames with underscore and period to get the "date and time"
     try:
         # block raising an exception
         f = f.split("_")[1].split(".")[0]
     except:
         continue # doing nothing on exception
-    #Slicing string to get only the date
-    f = f[0:8]
+
+    f = f[0:8]  #Slicing string to get only the date
     print(ogname)
     print(f)
     year = f[0:4]
@@ -36,3 +26,10 @@ for f in files:
         print("Folder created " + folder)
         shutil.move(ogname, folder)
         print("Moved " + ogname + " to " + folder)
+
+dir = input("Enter the complete path: ") + "\\"
+files = os.listdir(dir)
+
+
+for f in files:
+    organizeFiles(f)
