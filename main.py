2f943fd3a10560c483e8805f69a87d88fe391fc0
Ninad Pathak <20905629+ninadpathak@users.noreply.github.com>
2019-11-23 17:09:14 +0530

    Created a method and fixed the bugs

--- main.py
+++ main.py
@@ -1,31 +1,29 @@
 # This script is created to automatically organize the shitloads of call recordings that I have
 import os, time, shutil, calendar
-def organizeFiles(fileName):
+def organizeFiles(f):
     ogname = dir + f
-    print(ogname)
-
     #Splitting filenames with underscore and period to get the "date and time"
     try:
-        # block raising an exception
         f = f.split("_")[1].split(".")[0]
     except:
-        continue # doing nothing on exception
-
+        return
     f = f[0:8]  #Slicing string to get only the date
     print(ogname)
     print(f)
     year = f[0:4]
     month = f[4:6]
     day = f[6:8]
-    folder = dir + day + "-" + calendar.month_abbr[int(month)] + "-" + year
+    folder = dir + day + "-" + month + "-" + year
     if os.path.exists(folder):
         shutil.move(ogname, folder)
-        print("Moved " + f + " to " + folder)
+        print("Moved " + ogname + " to " + folder)
     else:
         os.mkdir(folder)
         print("Folder created " + folder)
         shutil.move(ogname, folder)
         print("Moved " + ogname + " to " + folder)
+
 dir = input("Enter the complete path: ") + "\\"
 files = os.listdir(dir)
 for f in files:
+    organizeFiles(f)
